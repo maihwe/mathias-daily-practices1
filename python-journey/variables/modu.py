b522cb92e2469b27a5665992864a178fe0975902
maihwe <ihwemathias@gmail.com>
2026-07-21 09:44:40 +0100

    variables

---
+++ python-journey/variables/modu.py
@@ -0,0 +1,13 @@
+from math import sin
+
+x = 3.14
+y = sin(x)
+print(y)
+
+from math import sin, cos
+
+x = 3.14
+y = sin(x)
+
+y = cos(x)
+print(y)
---
+++ python-journey/variables/var.py
@@ -0,0 +1,13 @@
+a = "Hello World"
+
+print(a)
+print(a[1])
+print(a[2:5])
+print(len(a))
+print(a.lower())
+print(a.upper())
+print(a.replace("H", "J"))
+print(a.split(" "))
+
+x = input("Enter your name: ")
+print("Hello, " + x)
