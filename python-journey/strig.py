44244299b177523bd603a6d534efaf5e51dfefce
maihwe <ihwemathias@gmail.com>
2026-07-23 17:07:38 +0100

    string practices

---
+++ python-journey/strig.py
@@ -0,0 +1,11 @@
+text = "The best thing in life are free"
+if "free" in text:
+    print("yes, free is present.")
+
+
+txt = "The best in life are free!"
+print("expensive" not in txt)
+
+test = "The best thing in life are free!"
+if "expensive" not in test:
+    print("No, expeensive is NOT present.")
